ac1c869d2641d8a0b7aa8e571d66eab0637346ee
Charles Onyango <charles.onyango@student.moringaschool.com>
2023-07-01 02:06:29 +0300

    complete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -27,3 +27,12 @@ class ReviewsController < ApplicationController
   end
 
 end
+def index
+  if params[:dog_house_id]
+    dog_house = DogHouse.find(params[:dog_house_id])
+    reviews = dog_house.reviews
+  else
+    reviews = Review.all
+  end
+  render json: reviews, include: :dog_house
+end
